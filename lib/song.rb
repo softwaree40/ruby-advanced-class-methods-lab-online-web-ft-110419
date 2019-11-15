5ed6e9256a04f0985868e68e846ac59cc8c4d0ce
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-15 18:50:49 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,37 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+  attr_reader :name 
   @@all = []
-
-  def self.all
+      
+  def self.new_by_name(name)
+      song = Song.new        #return song instance by taking in string name 
+      song.name = name 
+      song
+      #binding.pry
+   end
+   
+   def self.all
     @@all
-  end
+   end
 
-  def save
+   def save
     self.class.all << self
-  end
+   end
+   
+   def self.create
+     song = Song.new 
+     song.save
+     song 
+       
+   end
+   def self.create_by_name(name)
+     song = Song.new
+     song.name = name
+      @@all << song.name
+      
+     #binding.pry
+   end
+   
 
 end
