8e7fdde21cfef393356bcd9f344a9d025543d010
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-15 21:13:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,12 +37,13 @@ class Song
    end
     def self.find_or_create_by_name(title)
     #either return a matching song instance with that name or create a new song with the name and return the song instance
-    result = self.find_by_name(title)
+        result = self.find_by_name(title)
     if result
       result
     else
       self.create_by_name(title)
     end
+    
   end
 
   
