3bd373c72cac25e7a672d724794f471a703219a7
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-15 20:58:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,8 @@ class Song
        
      end
      def self.find_or_create_by_name(name)
-       if self.create_by_name(name) == self.find_by_name(name)  #invoke method to check if they are equal or not 
+       if self.create_by_name(name) == self.find_by_name(name)
+          binding.pry                                                               #invoke method to check if they are equal or not 
          return true 
        else
          false
