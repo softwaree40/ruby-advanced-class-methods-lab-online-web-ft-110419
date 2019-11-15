809b9eb701a1f5c2dd6ec3f7146767883e23e4a0
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-15 20:59:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,10 +42,9 @@ class Song
      def self.find_or_create_by_name(name)
        if self.create_by_name(name) == self.find_by_name(name)
           binding.pry                                                               #invoke method to check if they are equal or not 
-         return true 
-       else
-         false
          
+      
        end
+       return true 
     end
  end
