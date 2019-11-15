b99c9fcacf4b59b5071e33f5f19f752063006fca
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-15 20:01:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,8 @@ class Song
      
    end
      def self.find_by_name(name)
-       @@all.find {|song| song.name = name return name}
+       @@all.find {|song| song.name = name }
+       
      end
     
    
